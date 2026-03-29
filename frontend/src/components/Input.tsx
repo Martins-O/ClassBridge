a3a046b51817f36fedf4e539152f4188915b3862
Martins-O <jojololamartins686@gmail.com>
2026-03-29 12:00:00 +0100

    feat: add auth store and API service integration

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, ReactNode } from 'react';
+import type { InputHTMLAttributes } from 'react';
 import { clsx } from 'clsx';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
